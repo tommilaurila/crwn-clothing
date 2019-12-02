3590b45254aef87e38f6f74f2f9c1aed03768b81
Tommi Laurila <tommilaurila@gmail.com>
2019-12-02 20:00:14 +0200

    add withRouter

--- .history/src/components/directory/directory.component_20191202194936.jsx
+++ .history/src/components/directory/directory.component_20191202194936.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
-import { link } from 'fs';
 
 class Directory extends React.Component {
     constructor() {
@@ -15,7 +14,7 @@ class Directory extends React.Component {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
-                    linkUrl: 'hats'
+                    linkUrl: 'shop/hats'
                   },
                   {
                     title: 'jackets',
@@ -51,8 +50,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({ id, ...otherSectionProps }) => (
-                        <MenuItem key={id} {...otherSectionProps} />
+                    this.state.sections.map(({title, imageUrl, id, size}) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                     ))
                 }
             </div>
--- .history/src/index_20191202191246.js
+++ .history/src/index_20191202191246.js
@@ -4,12 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-
 ReactDOM.render(
 <BrowserRouter>
     <App />
-</BrowserRouter>, 
-document.getElementById('root')
-);
+</BrowserRouter>
+, document.getElementById('root'));
 
 
