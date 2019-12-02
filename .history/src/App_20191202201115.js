f7ddce7aec8f7d7bc60e6e080656c80d9d78d1e3
Tommi Laurila <tommilaurila@gmail.com>
2019-12-02 20:32:56 +0200

    added collection-preview page

--- .history/src/App_20191202201115.js
+++ .history/src/App_20191202201115.js
@@ -3,15 +3,20 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/sop.component.js';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+        <Route path='/hats' component={HatsPage} />
       </Switch>
     </div>
   );
