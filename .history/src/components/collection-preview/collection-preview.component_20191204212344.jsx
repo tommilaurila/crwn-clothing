5c6b7d9e21bd5e58c683aa98656b58deb03f3456
Tommi Laurila <tommilaurila@gmail.com>
2019-12-04 21:32:40 +0200

    updated shop page, collection preview, and item

--- .history/src/components/collection-preview/collection-preview.component_20191204212344.jsx
+++ .history/src/components/collection-preview/collection-preview.component_20191204212344.jsx
@@ -11,8 +11,8 @@ const CollectionPreview = ({ title, items }) => (
             {
                 items
                     .filter((item, idx) => idx < 4)
-                    .map(({ id, ...otherItemProps}) => (
-                    <CollectionItem key={id} {...otherItemProps} />
+                    .map(item => (
+                    <div key={item.id}>{item.name}</div>
             ))}
         </div>
     </div>
